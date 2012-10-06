3ac429e330ee01019606271a0d0fd5a19490669b
Bernd Gabriel <bernd.gabriel@fast-function-factory.de>
2012-10-06 18:26:11 +0200

    Added: new units to project files.

--- package/FrameViewer09.pas
+++ package/FrameViewer09.pas
@@ -2,23 +2,23 @@
   This source is only used to compile and install the package.
  }
 
-unit FrameViewer09; 
+unit FrameViewer09;
 
 interface
 
 uses
-    DitherUnit, FramBrwz, FrameViewerReg, FramView, HtmlBuffer, HtmlGif1, 
+  DitherUnit, FramBrwz, FrameViewerReg, FramView, HtmlBuffer, HtmlGif1, 
   HTMLGif2, HtmlGlobals, HtmlMisc, Htmlsbs1, HTMLSubs, HTMLUn2, HtmlView, 
   ReadHTML, StylePars, StyleUn, URLSubs, msimg32, AlphaBlendUn, HSLUtils, 
-  LazarusPackageIntf;
+  BuffConv, BuffConvArrays, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('FrameViewerReg', @FrameViewerReg.Register); 
-end; 
+  RegisterUnit('FrameViewerReg', @FrameViewerReg.Register);
+end;
 
 initialization
-  RegisterPackage('FrameViewer09', @Register); 
+  RegisterPackage('FrameViewer09', @Register);
 end.
