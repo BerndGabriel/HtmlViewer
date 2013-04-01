860dc851a361705a6a2a3cc1ac9e3e8fc6b3cc99
Bernd Gabriel <bernd.gabriel@fast-function-factory.de>
2013-04-01 15:39:16 +0200

    Simplified: methods darker() and Lighter().

--- source/HtmlGlobals.pas
+++ source/HtmlGlobals.pas
@@ -383,80 +383,70 @@ begin
   Result := GetDeviceCaps(ADC,SHADEBLENDCAPS) and SB_CONST_ALPHA > 0;
 end;
 
-
 function Darker(Color : TColor
   {$ifdef has_StyleElements}; const AUseThemes : Boolean{$endif}
   ): TColor; {$ifdef UseInline} inline; {$endif}
   {find a somewhat darker color for shading purposes}
 const
-  F =  66;
-  D = 100;
+  DarkerColors: array [0..255] of Byte = (
+      0,   1,   1,   2,   3,   3,   4,   4,   5,   6,   6,   7,   8,   8,   9,   9,
+     10,  11,  11,  12,  13,  13,  14,  14,  15,  16,  16,  17,  18,  18,  19,  19,
+     20,  21,  21,  22,  23,  23,  24,  24,  25,  26,  26,  27,  28,  28,  29,  29,
+     30,  31,  31,  32,  33,  33,  34,  35,  35,  36,  36,  37,  38,  38,  39,  40,
+     40,  41,  41,  42,  43,  43,  44,  45,  45,  46,  46,  47,  48,  48,  49,  50,
+     50,  51,  51,  52,  53,  53,  54,  55,  55,  56,  56,  57,  58,  58,  59,  60,
+     60,  61,  61,  62,  63,  63,  64,  65,  65,  66,  67,  67,  68,  68,  69,  70,
+     70,  71,  72,  72,  73,  73,  74,  75,  75,  76,  77,  77,  78,  78,  79,  80,
+     80,  81,  82,  82,  83,  83,  84,  85,  85,  86,  87,  87,  88,  88,  89,  90,
+     90,  91,  92,  92,  93,  93,  94,  95,  95,  96,  97,  97,  98,  99,  99, 100,
+    100, 101, 102, 102, 103, 104, 104, 105, 105, 106, 107, 107, 108, 109, 109, 110,
+    110, 111, 112, 112, 113, 114, 114, 115, 115, 116, 117, 117, 118, 119, 119, 120,
+    120, 121, 122, 122, 123, 124, 124, 125, 125, 126, 127, 127, 128, 129, 129, 130,
+    131, 131, 132, 132, 133, 134, 134, 135, 136, 136, 137, 137, 138, 139, 139, 140,
+    141, 141, 142, 142, 143, 144, 144, 145, 146, 146, 147, 147, 148, 149, 149, 150,
+    151, 151, 152, 152, 153, 154, 154, 155, 156, 156, 157, 157, 158, 159, 159, 160
+  );
 var
   Red, Green, Blue: Byte;
 begin
-// BG, 10.02.2013: clBtnHighlight used in Lighter() is white. Looks bad on white background.
-//  if Color = clBtnFace then
-//    Result := ThemedColor(clBtnShadow{$ifdef has_StyleElements},AUseThemes{$endif})
-//  else
-  begin
-    Result := ThemedColor(Color{$ifdef has_StyleElements},AUseThemes{$endif});
-    if Result <> 0 then
-    begin
-      Red   := Byte(Result       );
-      Green := Byte(Result shr  8);
-      Blue  := Byte(Result shr 16);
-
-      Result := RGB(MulDiv(Red, F, D), MulDiv(Green, F, D), MulDiv(Blue, F, D));
-    end;
-  end;
+  Result := ThemedColor(Color{$ifdef has_StyleElements},AUseThemes{$endif});
+  Red   := DarkerColors[Byte(Result       )];
+  Green := DarkerColors[Byte(Result shr  8)];
+  Blue  := DarkerColors[Byte(Result shr 16)];
+  Result := RGB(Red, Green, Blue);
 end;
-
 
 function Lighter(Color : TColor
   {$ifdef has_StyleElements}; const AUseThemes : Boolean {$endif})
   : TColor; {$ifdef UseInline} inline; {$endif}
 {find a somewhat lighter color for shading purposes}
-
-  function LighterEx(Color: Byte; var IsLight: Integer): Byte;
-  const
-    F = 125;
-    D = 100;
-    M = Byte((255 * 100) div F);
-  begin
-    if Color >= M then
-      Result := 255
-    else
-      Result := MulDiv(Color, F, D);
-    if Result >= $80 then
-      Inc(IsLight);
-  end;
-
 const
-  C0 = $A0A0A0;
-  C1 = $787878;
+  LighterColors: array [0..255] of Byte = (
+    128, 128, 129, 129, 130, 130, 131, 131, 132, 132, 133, 133, 134, 134, 135, 135,
+    136, 136, 137, 137, 138, 138, 139, 139, 140, 140, 141, 141, 142, 142, 143, 143,
+    144, 144, 145, 145, 146, 146, 147, 147, 148, 148, 149, 149, 150, 150, 151, 151,
+    152, 152, 153, 153, 154, 154, 155, 155, 156, 156, 157, 157, 158, 158, 159, 159,
+    160, 160, 161, 161, 162, 162, 163, 163, 164, 164, 165, 165, 166, 166, 167, 167,
+    168, 168, 169, 169, 170, 170, 171, 171, 172, 172, 173, 173, 174, 174, 175, 175,
+    176, 176, 177, 177, 178, 178, 179, 179, 180, 180, 181, 181, 182, 182, 183, 183,
+    184, 184, 185, 185, 186, 186, 187, 187, 188, 188, 189, 189, 190, 190, 191, 191,
+    192, 192, 193, 193, 194, 194, 195, 195, 196, 196, 197, 197, 198, 198, 199, 199,
+    200, 200, 201, 201, 202, 202, 203, 203, 204, 204, 205, 205, 206, 206, 207, 207,
+    208, 208, 209, 209, 210, 210, 211, 211, 212, 212, 213, 213, 214, 214, 215, 215,
+    216, 216, 217, 217, 218, 218, 219, 219, 220, 220, 221, 221, 222, 222, 223, 223,
+    224, 224, 225, 225, 226, 226, 227, 227, 228, 228, 229, 229, 230, 230, 231, 231,
+    232, 232, 233, 233, 234, 234, 235, 235, 236, 236, 237, 237, 238, 238, 239, 239,
+    240, 240, 241, 241, 242, 242, 243, 243, 244, 244, 245, 245, 246, 246, 247, 247,
+    248, 248, 249, 249, 250, 250, 251, 251, 252, 252, 253, 253, 254, 254, 255, 255
+  );
 var
   Red, Green, Blue: Byte;
-  IsLight: Integer;
 begin
-// BG, 10.02.2013: clBtnHighlight is white. Looks bad on white background.
-//  if Color = clBtnFace then
-//    Result := ThemedColor(clBtnHighlight{$ifdef has_StyleElements},AUseThemes{$endif})
-//  else
-  begin
-    Result := ThemedColor(Color{$ifdef has_StyleElements},AUseThemes{$endif});
-    if Result <> 0 then
-    begin
-      IsLight := 0;
-      Red   := LighterEx(Byte(Result       ), IsLight);
-      Green := LighterEx(Byte(Result shr  8), IsLight);
-      Blue  := LighterEx(Byte(Result shr 16), IsLight);
-      Result := RGB(Red, Green, Blue);
-      if IsLight > 0 then
-        Result := Result or C1;
-    end
-    else
-      Result := Result or C0;
-  end;
+  Result := ThemedColor(Color{$ifdef has_StyleElements},AUseThemes{$endif});
+  Red   := LighterColors[Byte(Result       )];
+  Green := LighterColors[Byte(Result shr  8)];
+  Blue  := LighterColors[Byte(Result shr 16)];
+  Result := RGB(Red, Green, Blue);
 end;
 
 function FindSpaces(PStart : PWideChar; const ACount : Integer) : Integer;
