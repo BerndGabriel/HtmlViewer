e0c097fc1750ecd578eb7425f3818cc50f38e881
michael van ham <michael.vanham@gmail.com>
2015-06-18 14:11:45 -0400

    add files from ../source to lazarus package

--- package/FrameViewer09.pas
+++ package/FrameViewer09.pas
@@ -10,7 +10,10 @@ uses
   DitherUnit, FramBrwz, FrameViewerReg, FramView, HtmlBuffer, HtmlGif1, 
   HTMLGif2, HtmlGlobals, HtmlMisc, Htmlsbs1, HTMLSubs, HTMLUn2, HtmlView, 
   ReadHTML, StylePars, StyleUn, URLSubs, msimg32, AlphaBlendUn, HSLUtils, 
-  BuffConv, BuffConvArrays, HtmlSymb, LazarusPackageIntf;
+  BuffConv, BuffConvArrays, HtmlSymb, BegaHtmlPrintPreviewForm, 
+  BegaMetaFilePrinter, BegaPreview, BegaPreviewForm, BegaPreviewPanel, 
+  BegaScrollBox, BegaZoom, GDIPL2A, HtmlCaches, HtmlFonts, HtmlImages, 
+  MetaFilePrinter, StyleTypes, vwPrint, WideStringsLcl, LazarusPackageIntf;
 
 implementation
 
