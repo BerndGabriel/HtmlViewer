BerndGabriel/HtmlViewer

commit 5dcb18290d630b46a205ea55086118c84a57b597

Fixed: github issue #102: GIF transparency inverted + memory leak

source/HtmlImages.pas
@@ -633,7 +633,7 @@ begin
       if Bitmap <> nil then
       begin
         //if Transparent = LLCorner then
-          Mask := GetImageMask(Bitmap, False, 0);
+          // Yunqa.de removed: Mask := GetImageMask(Bitmap, False, 0);
         Bitmap := ConvertImage(Bitmap);
         Result := ThtBitmapImage.Create(Bitmap, Mask, Transparent);
       end;

source/HTMLSubs.pas
@@ -2866,7 +2866,7 @@ begin
     H := ObjHeight;
   end;
   try
-    ddImage.Transp := Transparent;
+    // Yunqa.de removed: ddImage.Transp := Transparent;
     if IsCopy then
       ddImage.Print(Canvas, XX, Y, W, H, clWhite)
     else

commit 5959d51d0a4342d6a2c698e9e2ebf479b456d5f8

Fixed: commit 5dcb18290d630b46a205ea55086118c84a57b597 broke transparency for BMP files

source/HtmlImages.pas
@@ -632,8 +632,11 @@ begin
     if Result = nil then
       if Bitmap <> nil then
       begin
-        //if Transparent = LLCorner then
-          // Yunqa.de removed: Mask := GetImageMask(Bitmap, False, 0);
+        if not (ImageFormat in [itGif]) then
+        begin
+          Transparent := LLCorner;
+          Mask := GetImageMask(Bitmap, False, 0);
+        end;
         Bitmap := ConvertImage(Bitmap);
         Result := ThtBitmapImage.Create(Bitmap, Mask, Transparent);
       end;